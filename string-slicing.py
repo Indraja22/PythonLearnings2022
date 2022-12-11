f629612b2edc89cf9b57c466cea273b27e9dc76c
Indraja Naik <indraja9522@gmail.com>
2022-12-11 11:39:04 +0530

    Removed commented lines

--- string-slicing.py
+++ string-slicing.py
@@ -1,10 +1,10 @@
 userInputStr = input("Enter a string : ")
-# print(userInputStr[0:7])
-# print(userInputStr[0:6])
-# print(userInputStr[1:3])
-# print(userInputStr[-4:-2])
-# print(userInputStr[0:-6])
+print(userInputStr[0:7])
+print(userInputStr[0:6])
+print(userInputStr[1:3])
+print(userInputStr[-4:-2])
+print(userInputStr[0:-6])
 print(userInputStr[0:7:2])
 
-# for characters in userInputStr:
-#     print(characters)
+for characters in userInputStr:
+    print(characters)
